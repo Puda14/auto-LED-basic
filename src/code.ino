28b5765abc7f26fc98865d31163a8b4759c486cb
Phùng Tiến Đạt <96523185+Puda14@users.noreply.github.com>
2022-11-09 01:01:08 +0700

    Update code.ino

--- src/code.ino
+++ src/code.ino
@@ -2,9 +2,6 @@ const int LIGHT_SENSOR_PIN = A0; // Arduino pin connected to light sensor's  pin
 const int LED_PIN          = 3;  // Arduino pin connected to LED's pin
 const int ANALOG_THRESHOLD = 200;
 
-// variables will change:
-int analogValue;
-
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT); // set arduino pin to output mode
